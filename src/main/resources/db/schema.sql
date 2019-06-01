e08ca6813009e55577382653007057d5d9adf7ea
Imbirev Nikolay <imbirev.nikolay@gmail.com>
2019-06-01 18:18:24 +0300

    Sql init fixes (#22)
    * fix init sql script
    
    * fix init sql script v2

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,41 +1,39 @@
-CREATE SCHEMA IF NOT EXISTS pets_hotel (
-    CREATE TABLE IF NOT EXISTS bill (
-        id                BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        booking_id        BIGINT      NOT NULL,
-        usr_id            BIGINT      NOT NULL,
-        status            VARCHAR(6)  NOT NULL,
-        FOREIGN KEY (booking_id) references booking (id),
-        FOREIGN KEY (usr_id) references usr (id)
-    );
+CREATE TABLE IF NOT EXISTS USER (
+    id                BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    login             VARCHAR(50) NOT NULL,
+    password          VARCHAR(50) NOT NULL,
+    role              VARCHAR(5)  NOT NULL
+);
 
-    CREATE TABLE IF NOT EXISTS booking (
-        id                BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        room_id           BIGINT      NOT NULL,
-        start             DATE        NOT NULL,
-        endd              DATE        NOT NULL,
-        FOREIGN KEY (booking_id) references booking (id)
-    );
+CREATE TABLE IF NOT EXISTS HOTEL (
+    id                BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name              VARCHAR(50) NOT NULL,
+    location          VARCHAR(50) NOT NULL,
+    luxury            INT         NOT NULL
+);
 
-    CREATE TABLE IF NOT EXISTS hotel (
-        id                BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        name              VARCHAR(50) NOT NULL,
-        location          VARCHAR(50) NOT NULL,
-        luxury            INT         NOT NULL
-    );
+CREATE TABLE IF NOT EXISTS ROOM (
+    id              BIGINT     NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    hotel_id        BIGINT     NOT NULL,
+    num_of_guests   INTEGER    NOT NULL,
+    price_per_night INTEGER    NOT NULL,
+    class           VARCHAR(8) NOT NULL,
+    FOREIGN KEY (hotel_id) references HOTEL (id)
+);
 
-    CREATE TABLE IF NOT EXISTS room (
-        id                BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        hotel_id          BIGINT      NOT NULL,
-        num_of_guests     INTEGER     NOT NULL,
-        price_per_night   INTEGER     NOT NULL,
-        class             VARCHAR(8)  NOT NULL,
-        FOREIGN KEY (hotel_id) references hotel (id)
-    );
+CREATE TABLE IF NOT EXISTS BILL (
+    id                BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id           BIGINT      NOT NULL,
+    status            VARCHAR(7)  NOT NULL,
+    FOREIGN KEY (user_id) references USER (id)
+);
 
-    CREATE TABLE IF NOT EXISTS usr (
-        id                BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        login             VARCHAR(50) NOT NULL,
-        password          VARCHAR(50) NOT NULL,
-        role              VARCHAR(5)  NOT NULL
-    );
+CREATE TABLE IF NOT EXISTS BOOKING (
+    id                BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    room_id           BIGINT      NOT NULL,
+    bill_id           BIGINT      NOT NULL,
+    start_date        DATE        NOT NULL,
+    end_date          DATE        NOT NULL,
+    FOREIGN KEY (bill_id) references BILL (id),
+    FOREIGN KEY (room_id) references ROOM (id)
 );
