0b3e55d9e26ba2c9e86b9e429a542e3fd0fac285
alexeyboo <snfdrk@gmail.com>
2019-06-02 20:25:50 +0100

    added simple findById for HotelRepo

--- src/main/java/com/epam/Demo.java
+++ src/main/java/com/epam/Demo.java
@@ -1,9 +1,15 @@
 package com.epam;
 
+import com.epam.domain.Hotel;
+import com.epam.repo.HotelRepositoryImpl;
 import com.epam.utils.DBConnectionUtils;
 
 public class Demo {
     public static void main(String[] args) {
         DBConnectionUtils.getConnection();
+
+        HotelRepositoryImpl hotelRepository = new HotelRepositoryImpl();
+        Hotel hotel = hotelRepository.findById((long)1);
+        System.out.println(hotel.getLocation());
     }
 }
--- src/main/java/com/epam/repo/HotelRepositoryImpl.java
+++ src/main/java/com/epam/repo/HotelRepositoryImpl.java
@@ -1,8 +1,18 @@
 package com.epam.repo;
 
 import com.epam.domain.Hotel;
+import com.epam.domain.Room;
+import com.epam.utils.DBConnectionUtils;
+import lombok.SneakyThrows;
 
-public class HotelRepositoryImpl<T, K> implements Repository<Hotel, Long> {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+
+public class HotelRepositoryImpl implements Repository<Hotel, Long> {
+    Connection connection = DBConnectionUtils.getConnection();
+
     @Override
     public Hotel save(Hotel item) {
         return null;
@@ -14,8 +24,32 @@ public class HotelRepositoryImpl<T, K> implements Repository<Hotel, Long> {
     }
 
     @Override
+    @SneakyThrows
     public Hotel findById(Long id) {
-        return null;
+        String sql = "SELECT * FROM HOTEL WHERE ID = ";
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql + id);
+
+        String name = null;
+        String location = null;
+        int luxury = 0;
+
+        while (resultSet.next()) {
+            name = resultSet.getString("NAME");
+            location = resultSet.getNString("LOCATION");
+            luxury = resultSet.getInt("LUXURY");
+        }
+        List<Room> rooms = null;
+        Hotel.builder();
+        Hotel hotel = Hotel.builder().build();
+
+        hotel.setId(id);
+        hotel.setName(name);
+        hotel.setLocation(location);
+        hotel.setLuxury(luxury);
+        hotel.setRooms(rooms);
+
+        return hotel;
     }
 
     @Override
