2d531bfecf5b5950dde3e75c9f473caaaec9919a
Alexey Bondarev <snfdrk@gmail.com>
2019-06-03 21:45:37 +0300

    added javadocs to hotel&booking services

--- src/main/java/com/epam/service/BookingService.java
+++ src/main/java/com/epam/service/BookingService.java
@@ -5,46 +5,103 @@ import com.epam.repo.BookingRepositoryImpl;
 import lombok.SneakyThrows;
 
 public class BookingService {
+    BookingRepositoryImpl bookingRepository = new BookingRepositoryImpl();
+
+    /**
+     * Saves booking into the database
+     *
+     * @param booking
+     * @return Booking object that has been saved into the database
+     * @throws IllegalArgumentException
+     */
     @SneakyThrows
     public Booking save(Booking booking) throws IllegalArgumentException {
         return bookingRepository.save(booking);
     }
 
+    /**
+     * Removes Booking from the database by its id
+     *
+     * @param id
+     * @return Booking object that has been removed from the database
+     * @throws IllegalArgumentException
+     */
     @SneakyThrows
     public Booking removeById(Long id) throws IllegalArgumentException {
         return bookingRepository.removeById(id);
     }
 
+    /**
+     * Returns Booking from the database by its id
+     *
+     * @param id
+     * @return Booking object from the database by its id
+     * @throws IllegalArgumentException
+     */
     @SneakyThrows
     public Booking findById(Long id) throws IllegalArgumentException {
         return bookingRepository.findById(id);
     }
 
+    /**
+     * Updates Booking in the database
+     *
+     * @param booking
+     * @return Booking object that has been updated in the database
+     * @throws IllegalArgumentException
+     */
     @SneakyThrows
     public Booking update(Booking booking) throws IllegalArgumentException {
         return bookingRepository.update(booking);
     }
 
+    /**
+     * Finds all Bookings from database
+     *
+     * @return Iterable of all Booking objects in the database
+     */
     @SneakyThrows
     public Iterable<Booking> findAll() {
         return bookingRepository.findAll();
     }
 
+    /**
+     * Saves the array of Bookings into the database
+     *
+     * @param items
+     * @return Iterable of all Booking objects that have been saved in the database
+     */
     public Iterable<Booking> saveAll(Booking... items) {
         return bookingRepository.saveAll(items);
     }
 
+    /**
+     * Removes all Bookings from the database by their ids
+     *
+     * @param ids
+     * @return Iterable of all Booking objects that have been removed from the database by their ids
+     */
     public Iterable<Booking> removeAllById(Long... ids) {
         return bookingRepository.removeAllById(ids);
     }
 
+    /**
+     * Finds all Bookings from the database by their ids
+     *
+     * @param ids
+     * @return Iterable of all Booking objects by their ids
+     */
     public Iterable<Booking> findAllById(Long... ids) {
         return bookingRepository.findAllById(ids);
     }
 
-    public Iterable<Booking> updateAll(Booking... items) {
-        return bookingRepository.updateAll(items);
+    /**
+     * Updates all Bookings that have been passed into the method in the database
+     *
+     * @param bookings
+     * @return Iterable of all Bookings that have been updated in the database
+     */
+    public Iterable<Booking> updateAll(Booking... bookings) {
+        return bookingRepository.updateAll(bookings);
     }
-
-    BookingRepositoryImpl bookingRepository = new BookingRepositoryImpl();
 }
--- src/main/java/com/epam/service/HotelService.java
+++ src/main/java/com/epam/service/HotelService.java
@@ -7,44 +7,101 @@ import lombok.SneakyThrows;
 public class HotelService {
     HotelRepositoryImpl hotelRepository = new HotelRepositoryImpl();
 
+    /**
+     * Saves hotel into the database
+     *
+     * @param hotel
+     * @return Hotel object that has been saved into the database
+     * @throws IllegalArgumentException
+     */
     @SneakyThrows
     public Hotel save(Hotel hotel) throws IllegalArgumentException {
         return hotelRepository.save(hotel);
     }
 
+    /**
+     * Removes Hotel from the database by its id
+     *
+     * @param id
+     * @return Hotel object that has been removed from the database
+     * @throws IllegalArgumentException
+     */
     @SneakyThrows
     public Hotel removeById(Long id) throws IllegalArgumentException {
         return hotelRepository.removeById(id);
     }
 
+    /**
+     * Returns Hotel from the database by its id
+     *
+     * @param id
+     * @return Hotel object from the database by its id
+     * @throws IllegalArgumentException
+     */
     @SneakyThrows
     public Hotel findById(Long id) throws IllegalArgumentException {
         return hotelRepository.findById(id);
     }
 
+    /**
+     * Updates Hotel in the database
+     *
+     * @param hotel
+     * @return Hotel object that has been updated in the database
+     * @throws IllegalArgumentException
+     */
     @SneakyThrows
     public Hotel update(Hotel hotel) throws IllegalArgumentException {
         return hotelRepository.update(hotel);
     }
 
+    /**
+     * Finds all Hotels from database
+     *
+     * @return Iterable of all Hotel objects in the database
+     */
     @SneakyThrows
     public Iterable<Hotel> findAll() {
         return hotelRepository.findAll();
     }
 
-    public Iterable<Hotel> saveAll(Hotel... items) {
-        return hotelRepository.saveAll(items);
+    /**
+     * Saves the array of Hotels into the database
+     *
+     * @param hotels
+     * @return Iterable of all Hotel objects that have been saved in the database
+     */
+    public Iterable<Hotel> saveAll(Hotel... hotels) {
+        return hotelRepository.saveAll(hotels);
     }
 
+    /**
+     * Removes all Hotels from the database by their ids
+     *
+     * @param ids
+     * @return Iterable of all Hotel objects that have been removed from the database by their ids
+     */
     public Iterable<Hotel> removeAllById(Long... ids) {
         return hotelRepository.removeAllById(ids);
     }
 
+    /**
+     * Finds all Hotels from the database by their ids
+     *
+     * @param ids
+     * @return Iterable of all Hotel objects by their ids
+     */
     public Iterable<Hotel> findAllById(Long... ids) {
         return hotelRepository.findAllById(ids);
     }
 
-    public Iterable<Hotel> updateAll(Hotel... items) {
-        return hotelRepository.updateAll(items);
+    /**
+     * Updates all Hotel that have been passed into the method in the database
+     *
+     * @param hotels
+     * @return Iterable of all Hotels that have been updated in the database
+     */
+    public Iterable<Hotel> updateAll(Hotel... hotels) {
+        return hotelRepository.updateAll(hotels);
     }
 }
