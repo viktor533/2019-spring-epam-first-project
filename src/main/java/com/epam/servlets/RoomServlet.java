b5be472bc492a7398f72ca63173dbe333104a1b1
v.smirnov@unios.io <v.smirnov@unios.io>
2019-06-05 22:28:35 +0300

    Create Room page

--- src/main/java/com/epam/servlets/RoomServlet.java
+++ src/main/java/com/epam/servlets/RoomServlet.java
@@ -1,26 +1,63 @@
 package com.epam.servlets;
 
+import com.epam.domain.Booking;
 import com.epam.domain.Room;
+import com.epam.domain.enums.RoomClass;
+import com.epam.repo.Repository;
+import com.epam.state.RepositoryState;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RoomServlet extends BaseServlet  {
+    private static Repository<Room, Long> roomService = RepositoryState.getRoomRepositoryInstance();
+
+    private static Room getDemoRoom () {
+        List<Booking> bookings = new ArrayList<>();
+        bookings.add(Booking.builder()
+                .id(1)
+                .roomId(1)
+                .start(LocalDate.now())
+                .end(LocalDate.now().plusDays(3))
+                .build());
+
+        bookings.add(Booking.builder()
+                .id(2)
+                .roomId(1)
+                .start(LocalDate.now())
+                .end(LocalDate.now().plusDays(5))
+                .build());
+
+        Room room = Room.builder()
+                .id(1)
+                .hotelId(1)
+                .numOfGuests(3)
+                .pricePerNight(2000)
+                .roomClass(RoomClass.STANDART)
+                .bookings(bookings)
+                .build();
+
+        return room;
+    }
+
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String roomIdStr = request.getParameter("roomId");
-        int roomId = -1;
+        Long roomId = null;
         if (roomIdStr != null) {
-            roomId = Integer.parseInt(roomIdStr);
+            roomId = Long.parseLong(roomIdStr);
         }
         System.err.println("RoomId: " + roomId);
-//        Room room = RoomService.get(roomId);
-        Room room = null;
+//        Room room = roomService.findById(roomId);
+        Room room = getDemoRoom();
+        System.err.println("Room" + room);
         request.setAttribute("room", room);
         RequestDispatcher dispatcher = request.getRequestDispatcher("/pages/room_page.jsp");
         dispatcher.forward(request, response);
     }
-
 }
--- src/main/webapp/pages/room_page.jsp
+++ src/main/webapp/pages/room_page.jsp
@@ -6,11 +6,44 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html>
 <head>
     <title>Room</title>
+    <link rel='stylesheet' type='text/css' href='${pageContext.request.contextPath}/css/common.css'/>
+    <script>
+        function goBooking(id) {
+            location.href = '${pageContext.request.contextPath}/booking?bookingId='+id;
+        }
+    </script>
 </head>
 <body>
 
+    <dev class="centralCard">
+
+        <ul class="descriptionList">
+            <li> Room ${room.getId()} </li>
+            <li> <span>Location:</span> <em>${room.getNumOfGuests()}</em> </li>
+            <li> <span>Price:</span> <em>${room.getPricePerNight()}</em> </li>
+            <li> <span>Service:</span> <em>${room.getRoomClass()}</em> </li>
+        </ul>
+
+        <table class="table_grizzly">
+            <tr>
+                <th>ID</th>
+                <th>Start</th>
+                <th>End</th>
+            </tr>
+            <c:forEach items="${room.getBookings()}" var ="booking">
+                <tr>
+                    <td onclick="goBooking(${booking.getId()});">${booking.getId()}</td>
+                    <td onclick="goBooking(${booking.getId()});">${booking.getStart()}</td>
+                    <td onclick="goBooking(${booking.getId()});">${booking.getEnd()}</td>
+                </tr>
+            </c:forEach>
+        </table>
+
+    </dev>
 </body>
 </html>
