21f0ab3bcd1b99348b8ff4964871d8bd93ac6d1b
v.smirnov@unios.io <v.smirnov@unios.io>
2019-06-05 14:02:11 +0300

    Create Hotel Page

---
+++ src/main/java/com/epam/servlets/BaseServlet.java
@@ -0,0 +1,15 @@
+package com.epam.servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class BaseServlet extends HttpServlet {
+    protected void forward(String address, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(address);
+        dispatcher.forward(request, response);
+    }
+}
