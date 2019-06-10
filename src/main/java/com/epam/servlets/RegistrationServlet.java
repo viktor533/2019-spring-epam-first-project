88a0aecea522c49987172416a7055820226bcc03
Mincer2412 <artem4ik2412@gmail.com>
2019-06-10 12:25:51 +0300

    RegistrationServlet updated.

--- src/main/java/com/epam/servlets/RegistrationServlet.java
+++ src/main/java/com/epam/servlets/RegistrationServlet.java
@@ -4,7 +4,6 @@ import com.epam.domain.User;
 import com.epam.domain.enums.UserRole;
 import com.epam.service.UserService;
 import com.epam.state.RepositoryState;
-
 import java.io.IOException;
 import java.util.Random;
 import javax.servlet.RequestDispatcher;
@@ -16,30 +15,34 @@ import lombok.SneakyThrows;
 
 public class RegistrationServlet extends HttpServlet {
 
-  private static final UserService userService = new UserService(RepositoryState.getUserRepositoryInstance());
+    private static final UserService userService = new UserService(
+        RepositoryState.getUserRepositoryInstance());
 
-  @SneakyThrows
-  @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-    String email = request.getParameter("email");
-    String psw = request.getParameter("psw");
+    @SneakyThrows
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        String email = request.getParameter("email");
+        String psw = request.getParameter("psw");
 
-    User savedUser = userService.save(User.builder()
-        .login(email)
-        .password(psw)
-        .role(UserRole.USER)
-        .id(new Random().nextLong())
-        .build());
+        User savedUser = userService.save(User.builder()
+            .login(email)
+            .password(psw)
+            .role(UserRole.USER)
+            .id(new Random().nextLong())
+            .build());
 
-    if (savedUser != null) {
-      request.getRequestDispatcher("pages/successfulRegistration_page.jsp").forward(request, response);
-    } else {
-      request.getRequestDispatcher("pages/registration_page.jsp").forward(request, response);
+        if (savedUser != null) {
+            request.getRequestDispatcher("pages/successfulRegistration_page.jsp")
+                .forward(request, response);
+        } else {
+            request.getRequestDispatcher("pages/registration_page.jsp").forward(request, response);
+        }
     }
-  }
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    RequestDispatcher dispatcher = request.getRequestDispatcher("/pages/registration_page.jsp");
-    dispatcher.forward(request, response);
-  }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/pages/registration_page.jsp");
+        dispatcher.forward(request, response);
+    }
 
 }
